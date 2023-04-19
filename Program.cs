4fd499ebc33abe9b3b537f6251d64732b10d7095
Alexander Moreno <am@alexandermoreno.com>
2023-04-18 23:44:11 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+const string _clientId = "";
+const string _tenantId = "";
+
+var app = PublicClientApplicationBuilder
+	.Create(_clientId)
+	.WithAuthority(AzureCloudInstance.AzurePublic, _tenantId)
+	.WithRedirectUri("http://localhost")
+	.Build();
+
+string[] scopes = { "user.read"};
+AuthenticationResult result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+Console.WriteLine($"Token:\t{result.AccessToken}");
+
